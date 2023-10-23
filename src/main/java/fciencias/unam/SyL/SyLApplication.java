5bec5ef0f963acbde22596310ae1366171748475
Jose-MPM <jose_manuel@ciencias.unam.mx>
2023-10-23 17:22:36 -0600

    first commmit and spring init

---
+++ src/main/java/fciencias/unam/SyL/SyLApplication.java
@@ -0,0 +1,13 @@
+package fciencias.unam.SyL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyLApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fciencias/unam/SyL/SyLApplicationTests.java
@@ -0,0 +1,13 @@
+package fciencias.unam.SyL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
