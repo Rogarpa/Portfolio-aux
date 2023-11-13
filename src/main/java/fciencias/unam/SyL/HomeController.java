d11c27cd67c469f3dd5eff139e40439648627665
Rodriginsky <rodrigoestrategia2000@gmail.com>
2023-11-13 00:11:07 -0600

    [Feature] Add HomeController for inventario view 	ren src/main/java/fciencias/unam/SyL/Model.java -> src/main/java/fciencias/unam/         Confflict with name 	mod src/main/java/fciencias/unam/SyL/entity/Inventario.java         Inconsistent of data types with form in view 	mod src/main/java/fciencias/unam/SyL/service/InventarioService.java        Adding of functionality 	mod src/main/resources/templates/inventario.html         Enabled of parameters input in request

--- src/main/java/fciencias/unam/SyL/HomeController.java
+++ src/main/java/fciencias/unam/SyL/HomeController.java
@@ -1,33 +1,39 @@
 package fciencias.unam.SyL;
 
+
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import fciencias.unam.SyL.entity.Inventario;
+import fciencias.unam.SyL.repository.InventarioRepository;
 @Controller
-public class Model {
-
+public class HomeController {
+    @Autowired
+    private InventarioService service;
 
     @GetMapping("/AgregarProducto")
     public String agregarP(){
         return "agregarProducto";
     }
     @GetMapping("/inventario")
-    public String inventarios() {
+    public String inventarios(Model model) {
+        model.addAttribute("inventario", service.getInventarios());
         return "inventario";
     }
 
-   //@PostMapping("/guardar")
-    //public String save(@ModelAttribute Inventario inventario){
-    //    InventarioRepository.save(inventario);
+   @PostMapping("/guardar")
+    public String save(@ModelAttribute Inventario inventario){
+       service.saveInventario(inventario);
     //    InventarioRepository.flush();
-//
+
     //    this.mailSendr.sendSimpleMessage("ingrediente agregado");
-    //    return "redirect:/inventario"
-    //}
+       return "redirect:/inventario";
+    }
     
 }
--- src/main/java/fciencias/unam/SyL/service/InventarioService.java
+++ src/main/java/fciencias/unam/SyL/service/InventarioService.java
@@ -1,14 +1,23 @@
-package fciencias.unam.SyL.service;
+package fciencias.unam.SyL;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fciencias.unam.SyL.repository.InventarioRepository;
+import fciencias.unam.SyL.entity.Inventario;
 
+import java.util.List;
 @Service
 public class InventarioService {
 	@Autowired
     private InventarioRepository repository;
 
+    public List<Inventario> getInventarios() {
+        return repository.findAll();
+    }
+
+    public Inventario saveInventario(Inventario inventario) {
+        return repository.save(inventario);
+    }
 
 }
