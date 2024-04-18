16a8668e55dd40f381015f1c1c8bf204edf85919
Rodriginsky <rodrigoestrategia2000@gmail.com>
2024-04-18 13:11:03 -0600

    [feat]practica5: Add get-vars

--- practica5.rkt
+++ practica5.rkt
@@ -319,6 +319,191 @@
 
 
 
+(define (get-vars ir)
+        (nanopass-case (jelly Program) ir
+                [(program ,m) (let ([variables (mutable-set)])
+                                        (begin
+                                                (display "program-main-case: \n")
+                                                (print ir)
+                                                (display "\n")
+                                                (get-vars-Main m variables)
+                                                (display "\n")
+                                                variables))]
+                [(program ,m [,f* ... ,f]) (let ([variables (mutable-set)])
+                                                (begin
+                                                        (display "program-main-fun-case: \n")
+                                                        (print ir)
+                                                        (display "\n")
+                                                        (get-vars-Main m variables)
+                                                        (map (lambda (f) (get-vars-Function f variables)) f*)
+                                                        (get-vars-Function f variables)
+                                                        (display "\n")
+                                                        variables))]
+                [else (begin (display "caso Program \n")
+                        (print ir)
+                        (display "\n"))]))
+
+(define (get-vars-Main ir variables)
+        (nanopass-case (jelly Main) ir
+                [(main [,e* ... ,e]) (begin
+                                        (display "main-case: \n")
+                                        (print ir)
+                                        (display "\n")
+                                        (map (lambda (e) (get-vars-Expr e variables)) e*)
+                                        (get-vars-Expr e variables)
+                                        (display "\n"))]
+                [else (begin 
+                        (display "caso Main \n")
+                        (print ir)
+                        (display "\n"))]))
+
+(define (get-vars-Function ir variables) 
+        (nanopass-case (jelly Function) ir
+        [(,i ([,i* ,dt*] ...) ,t ,e) (begin
+                                        (display "function-type-case \n")
+                                        (print ir)
+                                        (display "\n")
+                                        (get-vars-Expr i variables)
+                                        (map (lambda (i) (get-vars-Expr i variables)) i*)
+                                        (map (lambda (dt) (get-vars-Expr dt variables)) dt*)
+                                        (get-vars-Expr t variables)
+                                        (get-vars-Expr e variables)
+                                        (display "\n"))]
+        [(,i ([,i* ,dt*] ...) ,e) (begin
+                                        (display "function-notype-case \n")
+                                        (print ir)
+                                        (display "\n")
+                                        (get-vars-Expr i variables)
+                                        (map (lambda (i) (get-vars-Expr i variables)) i*)
+                                        (map (lambda (dt) (get-vars-Expr dt variables)) dt*)
+                                        (get-vars-Expr e variables)
+                                        (display "\n"))]
+                [else (begin 
+                        (display "caso Function \n")
+                        (print ir)
+                        (display "\n"))]))
+
+(define (get-vars-DeclarationType ir variables) 
+        (nanopass-case (jelly DeclarationType) ir
+                [,t (begin
+                        (display "type-case \n")
+                        (print ir)
+                        (display "\n"))]
+                [(arrType ,t) (begin
+                                (display "arr-type-case \n")
+                                (print ir)
+                                (display "\n"))]
+                [else (begin 
+                        (display "caso DeclarationType \n")
+                        (print ir)
+                        (display "\n"))]))
+
+(define (get-vars-Expr ir variables) 
+        (nanopass-case (jelly Expr) ir
+                [,c (begin
+                                (display "expr-c-case \n")
+                                (print ir)
+                                (display "\n")
+                                (display "\n"))]
+                [,dt (begin
+                                (display "expr-dt-case \n")
+                                (print ir)
+                                (display "\n")
+                                (get-vars-DeclarationType dt variables)
+                                (display "\n"))]
+                [,pr (begin
+                                (display "expr-pr-case \n")
+                                (print ir)
+                                (display "\n")
+                                (display "\n"))]
+                [,i (begin
+                                (display "expr-i-case \n")
+                                (print ir)
+                                (set-add! variables i)
+                                (display "\n")
+                                (display "\n"))]
+                [(arrIndex ,e) (begin
+                                (display "expr-(arrIndex e)-case \n")
+                                (print ir)
+                                (display "\n")
+                                (get-vars-Expr e variables)
+                                (display "\n"))]
+                [(length ,e) (begin
+                                (display "expr-(length e)-case \n")
+                                (print ir)
+                                (display "\n")
+                                (get-vars-Expr e variables)
+                                (display "\n"))]
+                [(return ,e) (begin
+                                (display "expr-(return e)-case \n")
+                                (print ir)
+                                (display "\n")
+                                (get-vars-Expr e variables)
+                                (display "\n"))]
+                [(while ,e0 ,e1) (begin
+                                (display "expr-(while e0 e1)-case \n")
+                                (print ir)
+                                (display "\n")
+                                (get-vars-Expr e0 variables)
+                                (get-vars-Expr e1 variables)
+                                (display "\n"))]
+                [(if-stn ,e0 ,e1) (begin
+                                (display "expr-(if-stn e0 e1)-case \n")
+                                (print ir)
+                                (display "\n")
+                                (get-vars-Expr e0 variables)
+                                (get-vars-Expr e1 variables)
+                                (display "\n"))]
+                [(if-stn ,e0 ,e1 ,e2) (begin
+                                (display "expr-(if-stn e0 e1 e2)-case \n")
+                                (print ir)
+                                (display "\n")
+                                (get-vars-Expr e0 variables)
+                                (get-vars-Expr e1 variables)
+                                (get-vars-Expr e2 variables)
+                                (display "\n"))]
+                [(arrElement ,e1 ,e2) (begin
+                                (display "expr-(arrElement e1 e2)-case \n")
+                                (print ir)
+                                (display "\n")
+                                (get-vars-Expr e1 variables)
+                                (get-vars-Expr e2 variables)
+                                (display "\n"))]
+                [(decl ,i ,dt) (begin
+                                (display "expr-(decl i dt)-case \n")
+                                (print ir)
+                                (display "\n")
+                                (get-vars-Expr i variables)
+                                (get-vars-Expr dt variables)
+                                (display "\n"))]
+                [(,e* ...) (begin
+                                (display "expr-(e* ...)-case \n")
+                                (print ir)
+                                (display "\n")
+                                (map (lambda (e) (get-vars-Expr e variables)) e*)
+                                (display "\n"))]
+                [(,pr ,e0 ,e1) (begin
+                                (display "expr-(pr e0 e1)-case \n")
+                                (print ir)
+                                (display "\n")
+                                (get-vars-Expr pr variables)
+                                (get-vars-Expr e0 variables)
+                                (get-vars-Expr e1 variables)
+                                (display "\n"))]
+                [(,i [,e* ...]) (begin
+                                (display "expr-(i [e* ...])-case \n")
+                                (print ir)
+                                (display "\n")
+                                (get-vars-Expr i i)
+                                (map (lambda (e) (get-vars-Expr e variables)) e*)
+                                (display "\n"))]
+                [else (begin 
+                        (display "caso Expr \n")
+                        (print ir)
+                        (display "\n"))]))
+
+
+
 ; ENTRADA \\ENTRADA \\ENTRADA \\ENTRADA \\ENTRADA \\ENTRADA 
 ; ENTRADA \\ENTRADA \\ENTRADA \\ENTRADA \\ENTRADA \\ENTRADA 
 ; '(program 
