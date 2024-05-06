ba8dfbbfa4b69f1252c4039e64ecd2aa9b79eebc
Rodriginsky <rodrigoestrategia2000@gmail.com>
2024-05-06 15:33:02 -0600

    [feat]practica6: Add main sentences functionality

--- practica6.rkt
+++ practica6.rkt
@@ -22,9 +22,8 @@
 (define (get-type-Main ir table)
         (nanopass-case (jelly Main) ir
                 [(main [,e* ... ,e]) (begin
-                                        ; (map (lambda (e) (get-type-Expr e table)) e*)
+                                        (map (lambda (e) (get-type-Expr e table)) e*)
                                         (get-type-Expr e table)
-                                        (display (get-type-Expr e table))
                                         'unit)]
                 [else (begin 
                         (display "caso Main \n")
@@ -36,7 +35,7 @@
                                         ; (hash-set! table i (cons t (map declarationType->symbol dt*)))
                                         ; (map (lambda (id ty) (hash-set! table id (declarationType->symbol ty))) i* dt*)
                                         ; (get-type-Expr t table)
-                                        (display (get-type-Expr e table))
+                                        (get-type-Expr e table)
                                         'unit)]
         ; [(,i ([,i* ,dt*] ...) ,e) (begin
         ;                                 ; (map (lambda (id ty) (hash-set! table id (declarationType->symbol ty))) i* dt*)
@@ -103,12 +102,7 @@
                 ;                             'unit
                 ;                             (error "Do not exist type for arrElement " e2 "of" e1)))]
                 [(decl ,i ,dt) (hash-ref table i)]
-                ; [(,e* ...) (let*
-                ;                 ([types-list (map (lambda (e) (get-type-Expr e table)) e*)]
-                ;                 [is-same-type-list (foldr eq? (car types-list) (cdr types-list))])
-                ;                 (if is-same-type-list
-                ;                     (car types-list)
-                ;                     (error "Do not exist type for a heterogeneus list")))]
+                
                 [(,pr ,e0 ,e1) (let*
                                 ([e0-type (get-type-Expr e0 table)]
                                 [e1-type (get-type-Expr e1 table)])
@@ -123,13 +117,24 @@
                                                                     'bool
                                                                     (error "Do not exist type for boolean operation of non boolean expressions"))]
                                     [(memq pr '(=)) (cond 
-                                                        [(eq? e0-type e1-type) 'unit]
+                                                        [(eq? e0-type e1-type) e0-type]
                                                         [else (error "Do not exist type for assignation of distinct types")])]))]
-                
                 ; [(,i [,e* ...]) (begin
                 ;                 (get-type-Expr i i)
                 ;                 (map (lambda (e) (get-type-Expr e table)) e*)
                 ;                 table)]
+                
+                ;                 ([types-list (map (lambda (e) (get-type-Expr e table)) e*)]
+                ;                 [is-same-type-list (foldr eq? (car types-list) (cdr types-list))])
+                ;                 (if is-same-type-list
+                ;                     (car types-list)
+                ;                     (error "Do not exist type for a heterogeneus list")))]
+                [(,e* ...) (begin
+                                ; (map (lambda (e) (get-type-Expr e table)) e*)
+                                (display ir)
+                                (map (lambda (e) (display "\n")(display e)) e*)
+                                (map (lambda (e) (display "\n")(display (get-type-Expr e table))) e*)
+                                'unit)]
                 [else (begin 
                         (display "caso Expr \n")
                         (display ir)
@@ -141,43 +146,46 @@
         [else 'unit]))
 (define input-get-type (parser-jelly
     '(program
-    (main
-        (((=) (decl var_11 int) ((=) var_8 ((+) var_8 (1))))
-        ((=) var_8 ((+) var_8 (1)))
-        ((=) (decl var_10 int) (var_9 (var_11 var_12)))))
-    ((gdc
-        ((var_7 int) (var_6 int))
-        int
-        ((while
-        ((!=) var_7 (0))
-        ((if-stn ((<) var_7 var_6) (((=) var_6 ((-) var_6 var_7))))))
-        (return var_5)))
-        (sort
-        ((var_4 (arrType int)))
-        (((=) (decl var_3 int) (0))
-        ((=) (decl var_2 int) (length (var_4)))
-        (while
-        ((<) var_3 var_2)
-        (((=) (decl var_0 int) var_3)
+        (main
+            ((= (decl var_11 int) (= var_8 (+ var_8 (1))))
+            (= var_8 (+ var_8 (1)))
+            (= (decl var_10 int) (var_9 (var_11 var_12)))))
+        ((gdc
+            ((var_7 int) (var_6 int))
+            int
+            ((while
+            (!= var_7 (0))
+            ((if-stn (< var_7 var_6) ((= var_6 (- var_6 var_7))))))
+            (return var_5)))
+            (sort
+            ((var_4 (arrType int)))
+            ((= (decl var_3 int) (0))
+            (= (decl var_2 int) (length (var_4)))
             (while
-            ((>) var_0 (0))
-            ((if-stn
-            ((>)
-                (arrElement var_4 (arrIndex ((-) var_0 (1))))
-                (arrElement var_4 (arrIndex var_0)))
-            (((=) (decl var_1 int) (arrElement var_4 (arrIndex var_0)))
-                ((=)
-                (arrElement var_4 (arrIndex var_0))
-                (arrElement var_4 (arrIndex ((-) var_0 (1)))))
-                ((=) (arrElement var_4 (arrIndex ((-) var_0 (1)))) var_1)))
-            ((=) var_0 ((+) var_0 (1)))))
-            ((=) var_3 ((+) var_3 (1)))))))))))
+            (< var_3 var_2)
+            ((= (decl var_0 int) var_3)
+                (while
+                (> var_0 (0))
+                ((if-stn
+                (>
+                    (arrElement var_4 (arrIndex (- var_0 (1))))
+                    (arrElement var_4 (arrIndex var_0)))
+                ((= (decl var_1 int) (arrElement var_4 (arrIndex var_0)))
+                    (=
+                    (arrElement var_4 (arrIndex var_0))
+                    (arrElement var_4 (arrIndex (- var_0 (1)))))
+                    (= (arrElement var_4 (arrIndex (- var_0 (1)))) var_1)))
+                (= var_0 (+ var_0 (1)))))
+                (= var_3 (+ var_3 (1)))))))))))
 
 (define input-get-type2 (parser-jelly
     '(program
-   (main
-    ((= (decl var_0 int) 5)
-     )))))
+        (main
+                    (
+                        (= (decl var_0 int) (= var_2 (+ 1 1)))
+                    (= var_0 (+ var_0 1))
+                    (= (decl var_2 int) (+ var_2 (+ var_0 var_0)))
+                    )))))
 
 (define input-get-type2-table
     (make-hash '((var_0 . int)
