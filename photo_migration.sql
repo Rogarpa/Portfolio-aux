829164231940498bfd5c0e5bcc11433a45936cdf
Rogarpad <rodrigoestrategia2000@gmail.com>
2024-05-17 16:03:22 -0600

    [feat]photo_migration: Add inserting query

--- photo_migration.sql
+++ photo_migration.sql
@@ -1,34 +1,63 @@
--- Tomar id_empleado y foto 
-(SELECT id_empleado as id_empleado_foto, fotografia FROM det_empleado);
-
-
 -- Fotos a migrar con update
 (SELECT  fotografias_empleados.id_empleado_foto, fotografias_empleados.fotografia
 	FROM 
 		(SELECT id_empleado_foto, fotografia FROM det_empleado) AS fotografias_empleados
 		INNER JOIN det_empleado_foto ON fotografias_empleados.id_empleado_foto = det_empleado_foto.id_empleado_foto);
-
-UPDATE det_empleado_foto SET (SELECT  fotografias_empleados.id_empleado_foto, fotografias_empleados.fotografia
+-- Update Fotos
+UPDATE det_empleado_foto 
 	FROM 
-		(SELECT id_empleado_foto, fotografia FROM det_empleado) AS fotografias_empleados
-		INNER JOIN det_empleado_foto ON fotografias_empleados.id_empleado_foto = det_empleado_foto.id_empleado_foto);
-	
-	-- Fotos a migrar con insert
-SELECT *
-FROM det_empleado
-WHERE (id_empleado_foto IS NULL) OR (id_empleado_foto NOT IN (SELECT id_empleado_foto  FROM det_empleado_foto def));
+		(SELECT  fotografias_empleados.id_empleado_foto, fotografias_empleados.fotografia
+			FROM 
+			(SELECT id_empleado_foto, fotografia FROM det_empleado) AS fotografias_empleados
+			INNER JOIN det_empleado_foto ON fotografias_empleados.id_empleado_foto = det_empleado_foto.id_empleado_foto);
 
 
+-- -- Test update multiple 
+-- UPDATE det_empleado_foto 
+-- JOIN (SELECT  fotografias_empleados.id_empleado_foto, fotografias_empleados.fotografia
+-- 			FROM 
+-- 			(SELECT id_empleado_foto, fotografia FROM det_empleado) AS fotografias_empleados
+-- 			INNER JOIN det_empleado_foto ON fotografias_empleados.id_empleado_foto = det_empleado_foto.id_empleado_foto) AS norepeated
+-- ON id_empleado_foto 
+-- SET norepeated;
+
+-- Insert de fotos
+INSERT INTO det_empleado_foto (
+	SELECT det_empleado.id_empleado_foto, det_empleado.fotografia
+	FROM det_empleado
+	WHERE (id_empleado_foto IS NULL) OR (id_empleado_foto NOT IN (SELECT id_empleado_foto  FROM det_empleado_foto def))
+);
+
 	
 	
+-- Recuperar llaves det_empleado_foto
+WITH LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO AS (SELECT  * FROM det_empleado_foto)
+SELECT det_empleado.id_empleado, LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO.id_empleado_foto FROM LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO
+JOIN det_empleado
+ON det_empleado.fotografia = LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO.nb_fotografia;
+
+-- Update relacion llaves det_empleado con det_empleado_foto
+WITH LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO AS (SELECT  * FROM det_empleado_foto),
+RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO AS
+(
+	SELECT det_empleado.id_empleado, LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO.id_empleado_foto FROM LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO
+	JOIN det_empleado
+	ON det_empleado.fotografia = LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO.nb_fotografia
+)
+SELECT * FROM RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO;
+
+WITH LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO AS (SELECT  * FROM det_empleado_foto),
+RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO AS
+(
+	SELECT det_empleado.id_empleado, LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO.id_empleado_foto FROM LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO
+	JOIN det_empleado
+	ON det_empleado.fotografia = LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO.nb_fotografia
+)
+UPDATE det_empleado
+JOIN RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO ON det_empleado.id_empleado = RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO.id_empleado
+SET det_empleado.id_empleado_foto = RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO.id_empleado_foto;
 
 -- Insertion tests
-DELETE FROM det_empleado_foto WHERE id_empleado_foto = 2;
-INSERT INTO det_empleado_foto (id_empleado_foto, nb_fotografia) VALUES (3, "asdf");
-INSERT INTO det_empleado_foto (id_empleado_foto, nb_fotografia) VALUES (4, "asdf");
-
-START TRANSACTION;
-INSERT INTO det_empleado_foto (id_empleado_foto, nb_fotografia) VALUES (2, "asdf"); 
-SELECT LAST_INSERT_ID();
-COMMIT;
- 
+ALTER TABLE det_empleado_foto AUTO_INCREMENT=1;
+DELETE FROM det_empleado_foto WHERE id_empleado_foto <> 1;
+UPDATE det_empleado SET det_empleado.id_empleado_foto = NULL WHERE id_empleado <> 1;
