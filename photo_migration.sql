b7bacca2afc690fba2ac01f75b24b21d6b9343eb
Rogarpad <rodrigoestrategia2000@gmail.com>
2024-05-17 16:16:34 -0600

    [refact]photo_migration: Modularized queries

--- photo_migration.sql
+++ photo_migration.sql
@@ -1,40 +1,42 @@
 -- Fotos a migrar con update
-(SELECT  fotografias_empleados.id_empleado_foto, fotografias_empleados.fotografia
+(SELECT  FOTOGRAFIAS_EMPLEADOS.id_empleado_foto, FOTOGRAFIAS_EMPLEADOS.fotografia
 	FROM 
-		(SELECT id_empleado_foto, fotografia FROM det_empleado) AS fotografias_empleados
-		INNER JOIN det_empleado_foto ON fotografias_empleados.id_empleado_foto = det_empleado_foto.id_empleado_foto);
+		(SELECT id_empleado_foto, fotografia FROM det_empleado) AS FOTOGRAFIAS_EMPLEADOS
+		INNER JOIN det_empleado_foto ON FOTOGRAFIAS_EMPLEADOS.id_empleado_foto = det_empleado_foto.id_empleado_foto);
 -- Update Fotos
-UPDATE det_empleado_foto 
+-- UPDATE det_empleado_foto 
+-- 	FROM 
+-- 		(SELECT  FOTOGRAFIAS_EMPLEADOS.id_empleado_foto, FOTOGRAFIAS_EMPLEADOS.fotografia
+-- 			FROM 
+-- 			(SELECT id_empleado_foto, fotografia FROM det_empleado) AS FOTOGRAFIAS_EMPLEADOS
+-- 			INNER JOIN det_empleado_foto ON FOTOGRAFIAS_EMPLEADOS.id_empleado_foto = det_empleado_foto.id_empleado_foto);
+WITH
+EMPLEADOS_CON_ID_EMPLEADO_FOTO AS 
+(
+	SELECT  FOTOGRAFIAS_EMPLEADOS.id_empleado_foto, FOTOGRAFIAS_EMPLEADOS.fotografia
 	FROM 
-		(SELECT  fotografias_empleados.id_empleado_foto, fotografias_empleados.fotografia
-			FROM 
-			(SELECT id_empleado_foto, fotografia FROM det_empleado) AS fotografias_empleados
-			INNER JOIN det_empleado_foto ON fotografias_empleados.id_empleado_foto = det_empleado_foto.id_empleado_foto);
-
+		(SELECT id_empleado_foto, fotografia FROM det_empleado) AS FOTOGRAFIAS_EMPLEADOS
+	INNER JOIN det_empleado_foto ON FOTOGRAFIAS_EMPLEADOS.id_empleado_foto = det_empleado_foto.id_empleado_foto
+)
+UPDATE det_empleado_foto 
+JOIN EMPLEADOS_CON_ID_EMPLEADO_FOTO ON det_empleado_foto.id_empleado_foto = EMPLEADOS_CON_ID_EMPLEADO_FOTO.id_empleado_foto
+SET det_empleado_foto.nb_fotografia = EMPLEADOS_CON_ID_EMPLEADO_FOTO.fotografia;
 
 -- -- Test update multiple 
 -- UPDATE det_empleado_foto 
--- JOIN (SELECT  fotografias_empleados.id_empleado_foto, fotografias_empleados.fotografia
+-- JOIN (SELECT  FOTOGRAFIAS_EMPLEADOS.id_empleado_foto, FOTOGRAFIAS_EMPLEADOS.fotografia
 -- 			FROM 
--- 			(SELECT id_empleado_foto, fotografia FROM det_empleado) AS fotografias_empleados
--- 			INNER JOIN det_empleado_foto ON fotografias_empleados.id_empleado_foto = det_empleado_foto.id_empleado_foto) AS norepeated
+-- 			(SELECT id_empleado_foto, fotografia FROM det_empleado) AS FOTOGRAFIAS_EMPLEADOS
+-- 			INNER JOIN det_empleado_foto ON FOTOGRAFIAS_EMPLEADOS.id_empleado_foto = det_empleado_foto.id_empleado_foto) AS norepeated
 -- ON id_empleado_foto 
 -- SET norepeated;
 
--- Insert de fotos
+-- Clonacion de fotos existentes en det_empleado_foto
 INSERT INTO det_empleado_foto (
 	SELECT det_empleado.id_empleado_foto, det_empleado.fotografia
 	FROM det_empleado
 	WHERE (id_empleado_foto IS NULL) OR (id_empleado_foto NOT IN (SELECT id_empleado_foto  FROM det_empleado_foto def))
 );
-
-	
-	
--- Recuperar llaves det_empleado_foto
-WITH LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO AS (SELECT  * FROM det_empleado_foto)
-SELECT det_empleado.id_empleado, LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO.id_empleado_foto FROM LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO
-JOIN det_empleado
-ON det_empleado.fotografia = LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO.nb_fotografia;
 
 -- Update relacion llaves det_empleado con det_empleado_foto
 WITH LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO AS (SELECT  * FROM det_empleado_foto),
