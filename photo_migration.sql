ddb8dc8fdca06e13d127a2768ab974d9b585101f
Rogarpad <rodrigoestrategia2000@gmail.com>
2024-05-17 16:27:28 -0600

    [feat]photo_migration: Final migration queries

--- photo_migration.sql
+++ photo_migration.sql
@@ -1,15 +1,5 @@
--- Fotos a migrar con update
-(SELECT  FOTOGRAFIAS_EMPLEADOS.id_empleado_foto, FOTOGRAFIAS_EMPLEADOS.fotografia
-	FROM 
-		(SELECT id_empleado_foto, fotografia FROM det_empleado) AS FOTOGRAFIAS_EMPLEADOS
-		INNER JOIN det_empleado_foto ON FOTOGRAFIAS_EMPLEADOS.id_empleado_foto = det_empleado_foto.id_empleado_foto);
--- Update Fotos
--- UPDATE det_empleado_foto 
--- 	FROM 
--- 		(SELECT  FOTOGRAFIAS_EMPLEADOS.id_empleado_foto, FOTOGRAFIAS_EMPLEADOS.fotografia
--- 			FROM 
--- 			(SELECT id_empleado_foto, fotografia FROM det_empleado) AS FOTOGRAFIAS_EMPLEADOS
--- 			INNER JOIN det_empleado_foto ON FOTOGRAFIAS_EMPLEADOS.id_empleado_foto = det_empleado_foto.id_empleado_foto);
+-- Actualizacion de fotos existentes en det_empleado (con id_empleado_foto) a det_empleado_foto
+
 WITH
 EMPLEADOS_CON_ID_EMPLEADO_FOTO AS 
 (
@@ -22,16 +12,9 @@ UPDATE det_empleado_foto
 JOIN EMPLEADOS_CON_ID_EMPLEADO_FOTO ON det_empleado_foto.id_empleado_foto = EMPLEADOS_CON_ID_EMPLEADO_FOTO.id_empleado_foto
 SET det_empleado_foto.nb_fotografia = EMPLEADOS_CON_ID_EMPLEADO_FOTO.fotografia;
 
--- -- Test update multiple 
--- UPDATE det_empleado_foto 
--- JOIN (SELECT  FOTOGRAFIAS_EMPLEADOS.id_empleado_foto, FOTOGRAFIAS_EMPLEADOS.fotografia
--- 			FROM 
--- 			(SELECT id_empleado_foto, fotografia FROM det_empleado) AS FOTOGRAFIAS_EMPLEADOS
--- 			INNER JOIN det_empleado_foto ON FOTOGRAFIAS_EMPLEADOS.id_empleado_foto = det_empleado_foto.id_empleado_foto) AS norepeated
--- ON id_empleado_foto 
--- SET norepeated;
 
--- Clonacion de fotos existentes en det_empleado_foto
+
+-- Clonacion de fotos existentes en det_empleado (sin_id_empleado_foto) en det_empleado_foto
 INSERT INTO det_empleado_foto (
 	SELECT det_empleado.id_empleado_foto, det_empleado.fotografia
 	FROM det_empleado
@@ -39,14 +22,6 @@ INSERT INTO det_empleado_foto (
 );
 
 -- Update relacion llaves det_empleado con det_empleado_foto
-WITH LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO AS (SELECT  * FROM det_empleado_foto),
-RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO AS
-(
-	SELECT det_empleado.id_empleado, LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO.id_empleado_foto FROM LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO
-	JOIN det_empleado
-	ON det_empleado.fotografia = LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO.nb_fotografia
-)
-SELECT * FROM RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO;
 
 WITH LLAVES_FOTOS_INSERTADAS_DET_EMPLEADO_FOTO AS (SELECT  * FROM det_empleado_foto),
 RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO AS
@@ -59,7 +34,3 @@ UPDATE det_empleado
 JOIN RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO ON det_empleado.id_empleado = RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO.id_empleado
 SET det_empleado.id_empleado_foto = RELACION_LLAVES_DET_EMPLEADO_DET_EMPLEADO_FOTO.id_empleado_foto;
 
--- Insertion tests
-ALTER TABLE det_empleado_foto AUTO_INCREMENT=1;
-DELETE FROM det_empleado_foto WHERE id_empleado_foto <> 1;
-UPDATE det_empleado SET det_empleado.id_empleado_foto = NULL WHERE id_empleado <> 1;
