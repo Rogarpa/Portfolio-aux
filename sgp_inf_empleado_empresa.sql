16d4a73dd93612c83ab4a509672475f11a036318
Rogarpad <rodrigoestrategia2000@gmail.com>
2024-05-21 15:59:10 -0600

    [feat]keys-index: Add indexes table alteration

--- sgp_inf_empleado_empresa.sql
+++ sgp_inf_empleado_empresa.sql
@@ -1,6 +1,56 @@
-ALTER TABLE `inf_empleado_empresa`
-ADD COLUMN;
+ALTER TABLE inf_empleado_empresa
+KEY `fk_empleado_cat_cp` (`id_cp`),
 
+ALTER TABLE inf_empleado_empresa
+ADD CONSTRAINT `fk_det_empleado__category` FOREIGN KEY (`id_tabla`) REFERENCES `category` (`id_tabla`),;
+
+ALTER TABLE inf_empleado_empresa
+KEY `fk_empleado_cat_uso_cfdi` (`id_uso_cfdi`),
+
+ALTER TABLE inf_empleado_empresa
+ADD CONSTRAINT `fk_det_empleado__category` FOREIGN KEY (`id_tabla`) REFERENCES `category` (`id_tabla`),;
+
+
+ALTER TABLE inf_empleado_empresa
+KEY `fk_empleado_cat_sind` (`id_sind`),
+
+ALTER TABLE inf_empleado_empresa
+ADD CONSTRAINT `fk_det_empleado__category` FOREIGN KEY (`id_tabla`) REFERENCES `category` (`id_tabla`),;
+
+
+ALTER TABLE inf_empleado_empresa
+KEY `fk_empleado_cat_depto` (`id_depto`),
+
+ALTER TABLE inf_empleado_empresa
+ADD CONSTRAINT `fk_det_empleado__category` FOREIGN KEY (`id_tabla`) REFERENCES `category` (`id_tabla`),;
+
+
+ALTER TABLE inf_empleado_empresa
+KEY `fk_empleado_cat_riesgo_puesto` (`id_riesgo_puesto`),
+
+ALTER TABLE inf_empleado_empresa
+ADD CONSTRAINT `fk_det_empleado__category` FOREIGN KEY (`id_tabla`) REFERENCES `category` (`id_tabla`),;
+
+
+ALTER TABLE inf_empleado_empresa
+KEY `fk_empleado_cat_periodicidad` (`id_periodicidad`),
+
+ALTER TABLE inf_empleado_empresa
+ADD CONSTRAINT `fk_det_empleado__category` FOREIGN KEY (`id_tabla`) REFERENCES `category` (`id_tabla`),;
+
+
+ALTER TABLE inf_empleado_empresa
+KEY `fk_empleado_cat_salario_diario` (`id_salario_diario`),
+
+ALTER TABLE inf_empleado_empresa
+ADD CONSTRAINT `fk_det_empleado__category` FOREIGN KEY (`id_tabla`) REFERENCES `category` (`id_tabla`),;
+
+
+ALTER TABLE inf_empleado_empresa
+KEY `fk_empleado_cat_ent_fed` (`id_ent_fed`),
+
+ALTER TABLE inf_empleado_empresa
+ADD CONSTRAINT `fk_det_empleado__category` FOREIGN KEY (`id_tabla`) REFERENCES `category` (`id_tabla`),;
 
 -- Columnas faltantes de det_empleado==inf_empleado_empresa
 `nu_cp` varchar(10) NOT NULL,
